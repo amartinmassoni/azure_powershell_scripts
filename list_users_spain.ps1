1ffcec4531f99d14b320d2eb62e98ece12f59bda
Aurelio Martin <amartinmassoni@gmail.com>
2015-05-28 17:53:31 +0200

    Added -All to Get-MsolUser and several comments

--- list_users_spain.ps1
+++ list_users_spain.ps1
@@ -1,9 +1,14 @@
+# Connect to Microsoft Online
 
 Import-Module MSOnline
 
 Connect-MsolService
 
-$users = Get-MsolUser -Country Spain
+# Retrieve the list of all users in Spain
+
+$users = Get-MsolUser -All -Country Spain
+
+# Print in console the principal name of each user
 
 foreach ( $user in $users ) {
     Write-Host $user.UserPrincipalName
