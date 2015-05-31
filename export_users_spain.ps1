112cee4bf41502befbcc935416232225419d4112
Aurelio Martin <amartinmassoni@gmail.com>
2015-05-31 17:07:28 +0200

    Include the licenses Ids

--- export_users_spain.ps1
+++ export_users_spain.ps1
@@ -10,7 +10,7 @@ $users = Get-MsolUser -All -Country Spain
 
 # Create a new CSV file
 $file = New-Item c:\Temp\users.csv -type file -force
-Add-Content $file """Username"",""Name"""
+Add-Content $file """Username"",""Name"",""Licenses"""
 foreach ( $user in $users ) {
-    Add-Content $file """$($user.UserPrincipalName)"",""$($user.DisplayName)"""
+    Add-Content $file """$($user.UserPrincipalName)"",""$($user.DisplayName)"",""$($user.Licenses.AccountSkuId)"""
 }
